561707d9282e236443b78aa73277332e0356bcc4
tongpengdl <tongpengdl@gmail.com>
2025-12-06 23:55:36 -0800

    Make worker wait in a loop until being asked to return. All tests passed after this change

--- src/mr/worker.go
+++ src/mr/worker.go
@@ -4,10 +4,12 @@ import (
 	"encoding/json"
 	"fmt"
 	"hash/fnv"
+	"io"
 	"log"
 	"net/rpc"
 	"os"
 	"sort"
+	"time"
 )
 
 // Map functions return a slice of KeyValue.
@@ -31,95 +33,111 @@ func Worker(mapf func(string, string) []KeyValue,
 	// Your worker implementation here.
 
 	// uncomment to send the Example RPC to the coordinator.
-	task := GetTask()
-	switch task.Type {
-	case TaskTypeMap:
-		log.Printf("Starting Map Task %d on file %s\n", task.TaskNum, task.Filename)
-		// Read input file
-		content, err := os.ReadFile(task.Filename)
-		if err != nil {
-			log.Fatalf("cannot read %v", task.Filename)
-		}
-		// Call Map function
-		kva := mapf(task.Filename, string(content))
-
-		// Partition kva into nReduce intermediate files
-		buckets := make([][]KeyValue, task.NReduce)
-		for _, kv := range kva {
-			reduceTaskNum := ihash(kv.Key) % task.NReduce
-			buckets[reduceTaskNum] = append(buckets[reduceTaskNum], kv)
+	for {
+		task := GetTask()
+		if task == nil {
+			time.Sleep(time.Second)
+			continue
 		}
 
-		// Write intermediate files
-		for i := 0; i < task.NReduce; i++ {
-			intermediateFilename := fmt.Sprintf("mr-%d-%d", task.TaskNum, i)
-			file, err := os.Create(intermediateFilename)
+		switch task.Type {
+		case TaskTypeMap:
+			log.Printf("Starting Map Task %d on file %s\n", task.TaskNum, task.Filename)
+			// Read input file
+			content, err := os.ReadFile(task.Filename)
 			if err != nil {
-				log.Fatalf("cannot create %v", intermediateFilename)
+				log.Fatalf("cannot read %v", task.Filename)
 			}
-			enc := json.NewEncoder(file)
-			for _, kv := range buckets[i] {
-				if err := enc.Encode(&kv); err != nil {
-					log.Fatalf("cannot encode kv pair %v", kv)
+			// Call Map function
+			kva := mapf(task.Filename, string(content))
+
+			// Partition kva into nReduce intermediate files
+			buckets := make([][]KeyValue, task.NReduce)
+			for _, kv := range kva {
+				reduceTaskNum := ihash(kv.Key) % task.NReduce
+				buckets[reduceTaskNum] = append(buckets[reduceTaskNum], kv)
+			}
+
+			// Write intermediate files
+			for i := 0; i < task.NReduce; i++ {
+				intermediateFilename := fmt.Sprintf("mr-%d-%d", task.TaskNum, i)
+				file, err := os.Create(intermediateFilename)
+				if err != nil {
+					log.Fatalf("cannot create %v", intermediateFilename)
 				}
+				enc := json.NewEncoder(file)
+				for _, kv := range buckets[i] {
+					if err := enc.Encode(&kv); err != nil {
+						log.Fatalf("cannot encode kv pair %v", kv)
+					}
+				}
+				file.Close()
 			}
-			file.Close()
-		}
-		log.Printf("Completed Map Task %d\n", task.TaskNum)
-		ReportTaskCompletion(task)
-	case TaskTypeReduce:
-		log.Printf("Starting Reduce Task %d\n", task.TaskNum)
-		// Read intermediate files
-		intermediate := []KeyValue{}
-		for i := 0; i < task.NMap; i++ {
-			intermediateFilename := fmt.Sprintf("mr-%d-%d", i, task.TaskNum)
-			file, err := os.Open(intermediateFilename)
+			log.Printf("Completed Map Task %d\n", task.TaskNum)
+			ReportTaskCompletion(task)
+		case TaskTypeReduce:
+			log.Printf("Starting Reduce Task %d\n", task.TaskNum)
+			// Read intermediate files
+			intermediate := []KeyValue{}
+			for i := 0; i < task.NMap; i++ {
+				intermediateFilename := fmt.Sprintf("mr-%d-%d", i, task.TaskNum)
+				file, err := os.Open(intermediateFilename)
+				if err != nil {
+					log.Fatalf("cannot open %v", intermediateFilename)
+				}
+				dec := json.NewDecoder(file)
+				for {
+					var kv KeyValue
+					if err := dec.Decode(&kv); err != nil {
+						if err == io.EOF {
+							break
+						}
+						log.Fatalf("decode error reading %v: %v", intermediateFilename, err)
+					}
+					intermediate = append(intermediate, kv)
+				}
+				file.Close()
+			}
+
+			// Sort intermediate key-value pairs by key
+			sort.Slice(intermediate, func(i, j int) bool {
+				return intermediate[i].Key < intermediate[j].Key
+			})
+
+			// Create output file
+			outputFilename := fmt.Sprintf("mr-out-%d", task.TaskNum)
+			outputFile, err := os.Create(outputFilename)
 			if err != nil {
-				log.Fatalf("cannot open %v", intermediateFilename)
+				log.Fatalf("cannot create %v", outputFilename)
 			}
-			dec := json.NewDecoder(file)
-			for {
-				var kv KeyValue
-				if err := dec.Decode(&kv); err != nil {
-					break
+
+			// Apply Reduce function
+			i := 0
+			for i < len(intermediate) {
+				j := i + 1
+				for j < len(intermediate) && intermediate[j].Key == intermediate[i].Key {
+					j++
 				}
-				intermediate = append(intermediate, kv)
+				values := []string{}
+				for k := i; k < j; k++ {
+					values = append(values, intermediate[k].Value)
+				}
+				reducedValue := reducef(intermediate[i].Key, values)
+				fmt.Fprintf(outputFile, "%v %v\n", intermediate[i].Key, reducedValue)
+				i = j
 			}
-			file.Close()
+			outputFile.Close()
+			log.Printf("Completed Reduce Task %d\n", task.TaskNum)
+			ReportTaskCompletion(task)
+		case TaskTypeWait:
+			time.Sleep(time.Second)
+		case TaskTypeExit:
+			log.Printf("Received exit signal; worker shutting down\n")
+			return
+		default:
+			log.Printf("Received unknown task type: %v\n", task.Type)
+			time.Sleep(time.Second)
 		}
-
-		// Sort intermediate key-value pairs by key
-		sort.Slice(intermediate, func(i, j int) bool {
-			return intermediate[i].Key < intermediate[j].Key
-		})
-
-		// Create output file
-		outputFilename := fmt.Sprintf("mr-out-%d", task.TaskNum)
-		outputFile, err := os.Create(outputFilename)
-		if err != nil {
-			log.Fatalf("cannot create %v", outputFilename)
-		}
-
-		// Apply Reduce function
-		i := 0
-		for i < len(intermediate) {
-			j := i + 1
-			for j < len(intermediate) && intermediate[j].Key == intermediate[i].Key {
-				j++
-			}
-			values := []string{}
-			for k := i; k < j; k++ {
-				values = append(values, intermediate[k].Value)
-			}
-			reducedValue := reducef(intermediate[i].Key, values)
-			fmt.Fprintf(outputFile, "%v %v\n", intermediate[i].Key, reducedValue)
-			i = j
-		}
-		outputFile.Close()
-		log.Printf("Completed Reduce Task %d\n", task.TaskNum)
-		ReportTaskCompletion(task)
-	default:
-		log.Printf("Received unknown task type: %v\n", task.Type)
 	}
 }
 
